9f510f90da3b1deb755f7b26e67a3000a5d03c5d
Guimaraes Neto <guimaraesneto@gmail.com>
2025-02-09 16:15:57 -0300

    Implementado docker e docker-compose e ajustado o application.yml

--- report-gateway-service/src/test/java/br/com/getnet/reportgatewayservice/ReportGatewayServiceApplicationTests.java
+++ report-gateway-service/src/test/java/br/com/getnet/reportgatewayservice/ReportGatewayServiceApplicationTests.java
@@ -1,13 +1,7 @@
 package br.com.getnet.reportgatewayservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ReportGatewayServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
