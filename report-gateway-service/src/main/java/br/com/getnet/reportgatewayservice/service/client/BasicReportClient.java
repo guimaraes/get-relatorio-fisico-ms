4647b392ddf2ca3319f10f4e78858a6bad34a8eb
Guimaraes Neto <guimaraesneto@gmail.com>
2025-02-15 20:05:55 -0300

    Implementado o swagger

--- report-gateway-service/src/main/java/br/com/getnet/reportgatewayservice/service/client/BasicReportClient.java
+++ report-gateway-service/src/main/java/br/com/getnet/reportgatewayservice/service/client/BasicReportClient.java
@@ -1,21 +1,14 @@
 package br.com.getnet.reportgatewayservice.service.client;
 
 import br.com.getnet.reportgatewayservice.model.dto.ReportResponseDTO;
+import br.com.getnet.reportgatewayservice.config.ServiceProperties;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.beans.factory.annotation.Autowired;
 
-<<<<<<< HEAD
-@FeignClient(name = "basicReportClient", url = "#{@getBasicReportUrl}")
-=======
-@FeignClient(name = "basic-report-service")
->>>>>>> origin/development
+@FeignClient(name = "basicReportClient", url = "${services.basic-report-url}")
 public interface BasicReportClient {
     @GetMapping("/api/v1/basic-report/{cpf}")
     ReportResponseDTO getBasicReport(@PathVariable("cpf") String cpf);
 }
-
-
-
-
-
